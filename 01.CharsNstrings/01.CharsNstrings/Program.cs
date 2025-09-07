4e4deaa0e8d19a41b4412fa4e7ae9acfda694515
Byhu Victoria <vikabygu@gmail.com>
2025-09-07 22:42:12 +0300

    .

---
+++ 01.CharsNstrings/01.CharsNstrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+class CharsNstrings
+{
+    static void Main()
+    {
+        string a = "Chernivtsi National University";
+        char letter1 = 'B';
+        char letter2 = 'y';
+        char letter3 = 'e';
+        string b = "I love programming";
+
+        Console.WriteLine(a + "\n" + letter1 + "\n" + letter2 + "\n" + letter3 + "\n" + b);
+    }
+}
