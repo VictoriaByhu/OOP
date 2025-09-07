55a27baae15bfd654bd289bc276b9b1726866a93
Byhu Victoria <vikabygu@gmail.com>
2025-09-07 22:27:20 +0300

    .

---
+++ 01.Nums/01.Nums/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+class Nums
+{
+    static void Main()
+    {
+        int a = -100;
+        int b = 128;
+        int c = -3540;
+        int d = 64876;
+        long e = 2147483648;
+        long f = -1141583228;
+
+        Console.WriteLine(a + ", " + b + ", " + c + ", " + d + ", " + e + ", " + f);
+    }
+}
