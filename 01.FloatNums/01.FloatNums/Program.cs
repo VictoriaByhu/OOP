4e8d6b53b7c7f2c960ca79cc8f6329346bccf978
Byhu Victoria <vikabygu@gmail.com>
2025-09-07 22:40:38 +0300

    .

---
+++ 01.FloatNums/01.FloatNums/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+class FloatNums
+{
+    static void Main()
+    {
+        float a = -100;
+        float b = 128;
+        float c = -3540;
+        float d = 64876;
+        double e = 2147483648;
+        double f = -1141583228;
+
+        Console.WriteLine(a + " " + b + " " + c + " " + d + " " + e + " " + f);
+
+
+    }
+}
