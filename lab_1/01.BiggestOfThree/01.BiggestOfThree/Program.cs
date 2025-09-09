292841eaef089aae74604c9c7b3521da5493dd0f
Byhu Victoria <vikabygu@gmail.com>
2025-09-09 17:24:34 +0300

    .

---
+++ lab_1/01.BiggestOfThree/01.BiggestOfThree/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class BiggestOfThree
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter b: ");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter c: ");
+        int c = int.Parse(Console.ReadLine());
+
+        if (a > b && a > c)
+        {
+            Console.WriteLine("The biggest is: " + a);
+        }
+        else if (b > c && b > a)
+        {
+            Console.WriteLine("The biggest is: " + b);
+        }
+        else
+        {
+            Console.WriteLine("The biggest is: " + c);
+        }
+    }
+}
---
+++ lab_1/01.ProductSign/01.ProductSign/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+class ProductSign
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter b: ");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter c: ");
+        int c = int.Parse(Console.ReadLine());
+
+        if ()
+    }
+}
